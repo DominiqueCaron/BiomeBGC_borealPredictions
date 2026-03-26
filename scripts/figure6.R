0b204a660e77db62d190d60a6a5655ef1cde1996
DominiqueCaron <dominique.caron@mail.mcgill.ca>
2026-03-26 08:14:39 -0700

    make climaticControl map

--- scripts/figure6.R
+++ scripts/figure6.R
@@ -3,8 +3,10 @@
 # a) Map of limiting factors present (10-year average 2000-2010).
 # b) Map of limiting factors RCP 4.5 (10-year average 2000-2010). RCP4.5, average 3 scenarios
 # c) Map of limiting factors RCP 8.5 (10-year average 2090-2100). RCP8.5, average 3 scenarios
-source("fcnt4analysis.R")
+source("scripts/fcnt4analysis.R")
 library(reproducible)
+library(data.table)
+library(terra)
 library(sf)
 library(rnaturalearth)
 library(ggplot2)
@@ -13,9 +15,10 @@ library(ggpubr)
 library(ggtern)
 library(tricolore)
 
+out <- fread("data/processed/climate_bioclimatic_indices.csv")
 fig6a <- mapClimaticControl(out, eco_boreal, 2001:2010) + ggtitle("(a) Present") + theme(axis.text = element_blank(), axis.ticks = element_blank())
 fig6b <- mapClimaticControl(out, eco_boreal, 2091:2100, scenario = "RCP45") + ggtitle("(b) RCP 4.5") + theme(axis.text = element_blank(), axis.ticks = element_blank())
 fig6c <- mapClimaticControl(out, eco_boreal, 2091:2100, scenario = "RCP85") + ggtitle("(c) RCP 8.5") + theme(axis.text = element_blank(), axis.ticks = element_blank())
 
 fig6a / fig6b / fig6c
-ggsave("climaticControls.png", scale = 1)
+ggsave("figures/climaticControls.png", scale = 1)
